c57d9d13fa66ee504e66b51d23416700d1237f5d
Nikola Novak <nikola.novak.hr@gmail.com>
2021-04-01 01:07:59 +0200

    Changed text/background color & added animated form inputs

--- animated-login-form/src/App.svelte
+++ animated-login-form/src/App.svelte
@@ -1,30 +1,90 @@
 <script>
-	export let name;
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	form {
+		--text-color: #afafaf;
+		max-width: 500px;
+	}
+	.field {
+		width: 100%;
+		position: relative;
+		border-bottom: 2px dashed var(--text-color);
+		margin: 4rem auto 1rem;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.label {
+		color: var(--text-color);
+		font-size: 1.2rem;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.input {
+		outline: none;
+    border: none;
+    overflow: hidden;
+    margin: 0;
+    width: 100%;
+    padding: 0.25rem 0;
+    background: none;
+    color: white;
+    font-size: 1.2rem;
+    font-weight: bold;
+	}
+
+	.input:valid {
+		color: yellowgreen;
+	}
+
+	.input:invalid {
+		color: orangered;
+	}
+
+	.field::after {
+		content: "";
+		position: relative;
+		display: block;
+		height: 4px;
+		width: 100%;
+		background: #cf68ff;
+		transform: scaleX(0);
+		transform-origin: 0%;
+		transition: transform 500ms ease;
+		top: 2px;
+	}
+
+	.field:focus-within {
+		border-color: transparent;
+	}
+
+	.field:focus-within::after {
+		transform: scaleX(1);
+	}
+
+	.label {
+		z-index: -1;
+		position: absolute;
+		transform: translateY(-2rem);
+		transform-origin: 0%;
+		transition: transform 400ms;
+	}
+
+	.field:focus-within .label,
+	.input:not(:placeholder-shown) + .label {
+		transform: scale(0.8) translateY(-5rem);
 	}
 </style>
+
+<main>
+	<form>
+		<div class="field">
+			<input type="email" name="email" class="input" placeholder=" " />
+			<label for="email" class="label">Email</label>
+		</div>
+
+		<div class="field">
+			<input type="password" name="password" class="input" placeholder=" " />
+			<label for="password" class="label">Password</label>
+		</div>
+	</form>
+</main>
